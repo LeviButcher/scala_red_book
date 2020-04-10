dcb5c28ea6934dcc26006d8ee4181676f39d342c
Levi Butcher <levibutcher355@gmail.com>
2020-04-09 23:00:14 -0400

    State Transitions

--- scala_red_book/src/main/scala/chapter6/SimpleRNG.scala
+++ scala_red_book/src/main/scala/chapter6/SimpleRNG.scala
@@ -12,6 +12,45 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng =>
+      {
+        val (a, rng2) = ra(rng)
+        val (b, rng3) = rb(rng2)
+        (f(a, b), rng3)
+      }
+  }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0)
+        rng => (mod, rng)
+      else nonNegativeLessThan(n)
+    })
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (x, rng2) = rng.nextInt
     x match {
@@ -21,25 +60,18 @@ object RNG {
     }
   }
   def double(rng: RNG): (Double, RNG) = {
-    val (x, rng2) = rng.nextInt
-    val xx = 1 / x.toDouble
-    xx match {
-      case _ if x < 0 => (-xx, rng2)
-      case _          => (xx, rng2)
-      case 1          => (0, rng2)
-    }
+    map(nonNegativeInt)(i => 1 / i.toDouble)(rng)
   }
 
+  def int(rng: RNG): (Int, RNG) =
+    rng.nextInt
+
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (x, rng2) = rng.nextInt;
-    val (d, rng3) = RNG.double(rng2);
-    ((x, d), rng3)
+    both(int, double)(rng)
   }
 
   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val (x, rng2) = rng.nextInt;
-    val (d, rng3) = RNG.double(rng2);
-    ((d, x), rng3)
+    both(double, int)(rng)
   }
 
   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
