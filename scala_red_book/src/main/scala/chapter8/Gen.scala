50279f84567cfcfca83cc33612bdfb8f65d70a13
Levi Butcher <levibutcher355@gmail.com>
2020-04-16 22:57:04 -0400

    Half way there

--- scala_red_book/src/main/scala/chapter8/Gen.scala
+++ scala_red_book/src/main/scala/chapter8/Gen.scala
@@ -1,4 +1,12 @@
-case class Gen[A](sample: State.State[RNG, A])
+case class Gen[A](sample: State.State[RNG, A]) {
+  def flatMap[B](f: A => Gen[B]): Gen[B] = {
+    Gen.flatten(Gen[Gen[B]](RNG.map(this.sample)(f)))
+  }
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = {
+    size.flatMap(i => Gen.listOfN(i, this))
+  }
+}
 
 object Gen {
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
@@ -15,7 +23,73 @@ object Gen {
     )
   }
 
+  def flatten[A](a: Gen[Gen[A]]): Gen[A] = {
+    Gen[A](rng => {
+      val (g, rng2) = a.sample(rng)
+      g.sample(rng2)
+    })
+  }
+
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
     Gen[List[A]](RNG.sequence(List.fill(n)(g.sample)))
   }
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = {
+    val boolG = boolean
+    Gen[A](rng => {
+      val (b, rng2) = boolG.sample(rng)
+      b match {
+        case true  => g1.sample(rng2)
+        case false => g2.sample(rng2)
+      }
+    })
+  }
+
+  // def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+
+  // }
+}
+
+// trait Prop {
+//   def check: Either[(Prop.FailedCase, Prop.SuccessCount), Prop.SuccessCount]
+// }
+case class Prop(run: (Prop.TestCases, RNG) => Prop.Result)
+object Prop {
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+  case class Falsified(failure: FailedCase, successes: SuccessCount)
+      extends Result {
+    def isFalsified = true
+  }
+  type SuccessCount = Int
+  type TestCases = Int
+  type Result = Option[(Prop.FailedCase, Prop.SuccessCount)]
+  type FailedCase = String
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+    randomStream(as)(rng)
+      .zip(Stream.from(0))
+      .take(n)
+      .map {
+        case (a, i) =>
+          try {
+            if (f(a)) Passed else Falsified(a.toString, i)
+          } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+      }
+      .find(_.isFalsified)
+      .getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test ase: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTraec.mkString("\n")}"
+
 }
