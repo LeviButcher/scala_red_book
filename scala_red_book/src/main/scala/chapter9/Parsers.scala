42a298ceb4853bf0ad57478a81189b1d2728925b
Levi Butcher <levibutcher355@gmail.com>
2020-04-19 22:28:27 -0400

    Save 9 for later

--- scala_red_book/src/main/scala/chapter9/Parsers.scala
+++ scala_red_book/src/main/scala/chapter9/Parsers.scala
@@ -8,10 +8,15 @@ trait Parsers[ParseError, Parer[+_]] {
   ): ParserOps[String] = ParserOPs(f(a))
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
   def map[A, B](p: Parser[A], f: A => B)
-  def combine[A, B](p: Parser[A], p: Parser[B]): Parser[(A.B)]
+  def combine[A, B](p: Parser[A], p: Parser[B]): Parser[(A, B)]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def slice[A](p: Parser[A]): Parser[String]
+  def product[A, B](p: Parser[A], p: Parser[B]): Parser[(A, B)]
+  def map2[A, B, C](p: Parser[A], p: Parser[B])(f: (A, B) => C): Parser[C]
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    val numA: Parser[Int] = char('a').many.map(_.size)
   }
 }
