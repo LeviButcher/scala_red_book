e9ba32fc49a6a69e023a1296af8b51f876f6aac9
Levi Butcher <levibutcher355@gmail.com>
2020-04-24 23:49:00 -0400

    More Applicative

--- scala_red_book/src/main/scala/chapter12/Applicative.scala
+++ scala_red_book/src/main/scala/chapter12/Applicative.scala
@@ -1,6 +1,14 @@
 trait Applicative[F[_]] extends Functor[F] {
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: ((A, B) => C)): F[C]
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)((a, b) => a(b))
+
   def unit[A](a: => A): F[A]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: ((A, B) => C)): F[C] =
+    apply(map(fa)(f.curried))(fb)
+
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(map(fa)(f.curried))(fb))(fc)
 
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     map2(fa, unit(()))((a, _) => f(a))
